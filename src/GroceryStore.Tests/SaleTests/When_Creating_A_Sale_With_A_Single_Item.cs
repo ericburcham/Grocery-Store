c9418df431bc6daddabc5a85325b9d42cf71f21e
Eric Burcham <eburcham@eprod.com>
2017-05-11 11:00:11 -0500

    Finished dotfuscating all the things.

--- src/GroceryStore.Tests/SaleTests/When_Creating_A_Sale_With_A_Single_Item.cs
+++ src/GroceryStore.Tests/SaleTests/When_Creating_A_Sale_With_A_Single_Item.cs
@@ -1,30 +1,49 @@
-﻿//using System.Linq;
+﻿using System.Collections.Generic;
+using System.Linq;
+using FluentAssertions;
+using NUnit.Framework;
+using ProductData;
 
-//using FluentAssertions;
-//using GroceryStore.Domain;
-//using NUnit.Framework;
+namespace GroceryStore.Tests.SaleTests
+{
+    [TestFixture]
+    public class When_Creating_A_Sale_With_A_Single_Item
+    {
+        private LineItem _lineItem;
 
-//namespace GroceryStore.Tests.SaleTests
-//{
-//    [TestFixture]
-//    public class When_Creating_A_Sale_With_A_Single_Item
-//    {
-//        private LineItem _lineItem;
+        private string _productSku;
 
-//        private Sale _sale;
+        private string _productDescription;
 
-//        [OneTimeSetUp]
-//        public void OneTimeSetUp()
-//        {
-//            _sale = new Sale();
-//            _sale.AddItem("1245");
-//            _lineItem = _sale.LineItems.Single();
-//        }
+        private decimal _productPrice;
 
-//        [Test]
-//        public void There_Should_Only_Be_One_LineItem()
-//        {
-//            _sale.LineItems.Count.Should().Be(1);
-//        }
-//    }
-//}
+        private Sale _sale;
+
+
+        [OneTimeSetUp]
+        public void OneTimeSetUp()
+        {
+            var productDataProvider = new ProductDataProvider();
+
+            GetProductionData(productDataProvider);
+
+            _sale = new Sale();
+            _sale.AddItem("1245");
+            _lineItem = _sale.LineItems.Single();
+        }
+
+        [Test]
+        public void The_LineItem_Price_Should_Be_Correct()
+        {
+            _lineItem.Price.Should().Be(_productPrice);
+        }
+
+        private void GetProductionData(ProductDataProvider productDataProvider)
+        {
+            var productData = productDataProvider.GetProductData("1245");
+            _productSku = productData[0];
+            _productDescription = productData[1];
+            _productPrice = decimal.Parse(productData[2]);
+        }
+    }
+}
